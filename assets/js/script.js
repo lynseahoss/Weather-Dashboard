591906931bd6e44ec839aabf73186d9426759877
Lindsey Lawson <lynseahoss@gmail.com>
2020-05-13 18:31:31 -0400

    update JS & HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,81 +3,87 @@ var cities = [""];
 
 var cityStorage = JSON.parse(localStorage.getItem("cities")) || [];
 
-
-
 //Functions
 
 //
 
 //call current city weather API
-function displayCurrentWeather() {
-  var cityData = $(this).attr("data-city")
+function displayCurrentWeather(cityData) {
+
   var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + cityData + "&units=imperial&APPID=ae1dd4475c7b60eeeab3ef88607506d0";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityData +
+    "&units=imperial&APPID=" + apiKey;
   $.ajax({
     url: queryURL,
-    method: "GET",
-  }).then(function (response) {
+    method: "GET"
+  }).then(function(response) {
     console.log(response);
-    console.log(response.name);
+   // console.log(response.name);
     //date variable
-    var date = new Date(response.dt*1000);
+    var date = moment().format("l");  
     //icon for weather info
-    var weatherImg = "https://openweathermap.org/img/wn/$response.weather[0].icon@2x.png"
-    var wthrDiv = $("#present-city");
-    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-    wthrDiv.append($("<h4>").text(response.nameresponse.name+(date.getMonth()+1)+ (date.getFullYear())))        
-    wthrDiv.append($("<p>").text("Temperature: " + tempF.toFixed(2)))
-    wthrDiv.append($("<p>").text("Wind Speed: " + response.wind.speed))
-    wthrDiv.append($("<>").text("Humidity: " + response.main.humidity + "%"))
-    $("#present-city").prepend(wthrDiv)
+    var weatherImg = $("<img>").attr("src","https://openweathermap.org/img/wn/"+"03d"+"@2x.png");
+    var wthrDiv = $("<div>");
+    var tempF = response.main.temp;
+    console.log(weatherImg)
+    
+    var newH4 =  $("<h4>").text(response.name+" ("+date+") ")
+    newH4.append(weatherImg)
+
+    wthrDiv.append(newH4)
+    wthrDiv.append($("<p>").text("Temperature: " + tempF.toFixed(2)));
+    wthrDiv.append($("<p>").text("Wind Speed: " + response.wind.speed));
+    wthrDiv.append($("<p>").text("Humidity: " + response.main.humidity + "%"));
+    $("#present-city").prepend(wthrDiv);
   });
 }
 
-function renderCity(){
+function renderCity() {
   $("#city-list").empty();
-for (var i= 0; i < movies.length; i++){
-  var x = $("<li>")
-  x.addClass("li-city")
-  x.attr("data-city", cities[i])
-  x.text(cities[i])
-  $("city-list").append(x)
+  console.log(cityStorage)
+  if (cityStorage.length === 0) return;
+  for (var i = 0; i < cityStorage.length; i++) {
+    var newLi= $("<li>");
+    newLi.addClass("li-city list-group-item");
+    newLi.attr("data-city", cityStorage[i]);
+    newLi.text(cityStorage[i]);
+    $("#city-list").prepend(newLi);
 
-  //$("#city-list").append($("<li class = 'city-li'>")).text(cityStorage[i])
-}
-}
-
-
-  $("#find-city").on("click", function (event) {
-    //   //prevents from resetting page
-      event.preventDefault();
-      var cityForm = $("#city-input").val().trim();
-      cityStorage.push(cityForm);
-      localStorage.setItem("cities", JSON.stringify(cityStorage));
-      $("#city-input").val("");
+    //$("#city-list").append($("<li class = 'city-li'>")).text(cityStorage[i])
+  }
+  displayCurrentWeather(cityStorage[cityStorage.length-1]);
 }
 
+$("#find-city").on("click", function(event) {
+  //   //prevents from resetting page
+  event.preventDefault();
+  var cityForm = $("#city-input")
+    .val()
+    .trim();
+  cityStorage.push(cityForm);
+  localStorage.setItem("cities", JSON.stringify(cityStorage));
+  $("#city-input").val("");
+  renderCity()
+});
 
-function fiveDayForecast(){
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&APPID=ae1dd4475c7b60eeeab3ef88607506d0"
-$.ajax({
+function fiveDayForecast(inputCity, inputState) {
+  var queryURL =
+  "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + inputCity + "," + inputState + ",US&APPID=" + apiKey
+  $.ajax({
     url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    
-    
-  });
-}
+    method: "GET"
+  }).then(function(response) {
+    console.log(response)
+})
+  }
 
 
-
-
-$("#fuckThis").on("click", ".city-li", function () {
-  console.log($(this).text());
+$("#city-list").on("click", ".li-city", function() {
+  displayCurrentWeather($(this).attr("data-city"))
 });
 renderCity();
-displayCurrentWeather()
+console.log(fiveDayForecast("Reno","NV"))
 
 
 
@@ -101,7 +107,6 @@ displayCurrentWeather()
 //   renderCity();
 //   displayCurrentWeather()
 // });
-
 
 // $("#find-city").on("click", function (event) {
 //     event.preventDefault();
