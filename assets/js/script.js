e186104401d78966a59736c4eeeb5a02f0c4bc49
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-05-12 22:53:58 -0400

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,49 @@
 
 var apiKey = "&ae1dd4475c7b60eeeab3ef88607506d0"
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
-var citySearch = JSON.parse(localStorage.getItem("citySearch")) || [];        
+var cityStorage = JSON.parse(localStorage.getItem("citySearch")) || [];        
 var cities = [""]
 
 
 
 //Functions
-function displayWeather(){
-    var weatherAttr = $(this).attr("data-name")
-   var cityURL = queryURL + weatherAttr + apiKey
+// function displayWeather(){
+//     var weatherAttr = $(this).attr("data-name")
+//    var cityURL = queryURL + weatherAttr + apiKey
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function(response){
-        console.log(response)
-var wthrDiv = $("<section>")
-wthrDiv.append($("<p>").text("City: " + response.city.name))
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//     }).then(function(response){
+//         console.log(response)
+// var wthrDiv = $("<section>")
+// wthrDiv.append($("<p>").text("City: " + response.city.name))
 
-$(".weather-view").prepend(wthrDiv)
-    })
+// $(".weather-view").prepend(wthrDiv)
+//     })
 
-}
+// }
 
 function renderCity(){
     $("#city-view").empty
     for (var i = 0; i <cities.length; i++){
-       $("#city-view").append($("<p class = 'city'>")).text(citySearch[i])
+       $("#city-list").append($("<li class = 'city-li'>")).text(citySearch[i])
     }
 }
+$("form").on("submit", function(event){
+    //prevents from resetting page
+    event.preventDefault()
+    var cityForm = $("#city").val().trim()
+    localStorage.setItem("cityStorage", JSON.stringify(cityStorage))
+    $("#city").val("")
 
-$("#find-city").on("click", function (event) {
-    event.preventDefault();
-var cityInput = $("city-input").val()
-cities.push(cityInput)
+    renderCity()
 
 })
+
+// $("#find-city").on("click", function (event) {
+//     event.preventDefault();
+// var cityInput = $("city-input").val()
+// cities.push(cityInput)
+
+// })
