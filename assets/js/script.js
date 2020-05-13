d530e788fc2cd37d88259e008a53941c60195656
Lindsey Lawson <lindseylawson@Lindseys-MacBook-Pro.local>
2020-05-12 23:07:37 -0400

    update JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
-var apiKey = "&ae1dd4475c7b60eeeab3ef88607506d0"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
+// var apiKey = "&ae1dd4475c7b60eeeab3ef88607506d0"
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
 var cityStorage = JSON.parse(localStorage.getItem("citySearch")) || [];        
 var cities = [""]
 
@@ -25,21 +25,28 @@ var cities = [""]
 // }
 
 function renderCity(){
-    $("#city-view").empty
+    $("#city-view").empty()
     for (var i = 0; i <cities.length; i++){
-       $("#city-list").append($("<li class = 'city-li'>")).text(citySearch[i])
+       $("#city-list").append($("<p class = 'city-li'>")).text(cityStorage[i])
     }
+    console.log(renderCity)
 }
 $("form").on("submit", function(event){
     //prevents from resetting page
     event.preventDefault()
-    var cityForm = $("#city").val().trim()
+    var cityForm = $("#city").val()
+    cityStorage.push(cityForm)
     localStorage.setItem("cityStorage", JSON.stringify(cityStorage))
     $("#city").val("")
 
     renderCity()
 
 })
+
+$(document).on("click", ".city", function(){
+    console.log($(this).text())
+})
+renderCity()
 
 // $("#find-city").on("click", function (event) {
 //     event.preventDefault();
