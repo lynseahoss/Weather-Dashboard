fd09d768bf6eec374f5b6e551b9906b3e073b8e2
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-05-13 00:35:40 -0400

    update html & css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,52 @@
 
-// var apiKey = "&ae1dd4475c7b60eeeab3ef88607506d0"
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
+var apiKey = "&ae1dd4475c7b60eeeab3ef88607506d0"
+var cities = $("#city-input")
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cities+ apiKey
 var cityStorage = JSON.parse(localStorage.getItem("citySearch")) || [];        
-var cities = [""]
+var cities = $("#city-input")
 
 
 
 //Functions
-// function displayWeather(){
-//     var weatherAttr = $(this).attr("data-name")
-//    var cityURL = queryURL + weatherAttr + apiKey
+function displayWeather(){
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response){
+        console.log(response)
+        console.log(response.name)
+var wthrDiv = $("#present-city")
+wthrDiv.append($("<p>").text("City: " + response.city.name))
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//     }).then(function(response){
-//         console.log(response)
-// var wthrDiv = $("<section>")
-// wthrDiv.append($("<p>").text("City: " + response.city.name))
+$(".weather-view").prepend(wthrDiv)
+    })
 
-// $(".weather-view").prepend(wthrDiv)
-//     })
-
-// }
-
-function renderCity(){
-    $("#city-view").empty()
-    for (var i = 0; i <cities.length; i++){
-       $("#city-list").append($("<p class = 'city-li'>")).text(cityStorage[i])
-    }
-    console.log(renderCity)
 }
-$("form").on("submit", function(event){
-    //prevents from resetting page
-    event.preventDefault()
-    var cityForm = $("#city").val()
-    cityStorage.push(cityForm)
-    localStorage.setItem("cityStorage", JSON.stringify(cityStorage))
-    $("#city").val("")
 
-    renderCity()
+// function displayCity(){
+//     $("#city-view").empty()
+//     for (var i = 0; i <cities.length; i++){
+//        $("#city-list").append($("<p class = 'city-li'>")).text(cityStorage[i])
+//     }
+//     console.log(renderCity)
+// }
+// $("form").on("submit", function(event){
+//     //prevents from resetting page
+//     event.preventDefault()
+//     var cityForm = $("#city").val()
+//     cityStorage.push(cityForm)
+//     localStorage.setItem("cityStorage", JSON.stringify(cityStorage))
+//     $("#city").val("")
 
-})
+//     displayCity()
 
-$(document).on("click", ".city", function(){
-    console.log($(this).text())
-})
-renderCity()
+// })
+
+// $(document).on("click", ".city", function(){
+//     console.log($(this).text())
+// })
+// displayCity()
 
 // $("#find-city").on("click", function (event) {
 //     event.preventDefault();
